97b0e56c7969b8fd09b9702521f6de4cf34d0b2b
kowalks <guilhermekowalczuk@gmail.com>
2021-08-20 09:32:19 -0300

    Added iterations

--- weisfeiler-lehman.py
+++ weisfeiler-lehman.py
@@ -1,7 +1,24 @@
 import networkx as nx
 
 
-def weisfeiler_lehman_test(G, H):
+def weisfeiler_lehman_iter(G:nx.Graph):
+    # Iterando em cada nó e verificando o label dos vizinhos
+    mset = {}
+    for node in G.nodes:
+        mset[node] = ()
+        for ng in G[node]:
+            mset[node] += (G.nodes[ng]['label'],)
+
+    # Iterando pelos multisets criados e fazndo o hash
+    for node in G.nodes:
+        mset[node] = hash(mset[node])
+
+    # Atribuindo novo label para cada nó
+    for node in G.nodes:
+        G.nodes[node]['label'] = mset[node]
+
+
+def weisfeiler_lehman_test(G:nx.Graph, H:nx.Graph, k: int):
     # Sanity check
     if len(G.nodes) != len(H.nodes) | len(G.edges) != len(G.edges):
         return False
@@ -9,10 +26,19 @@ def weisfeiler_lehman_test(G, H):
     # Atribuindo os graus como labels iniciais
     for node in G.nodes:
         G.nodes[node]['label'] = G.degree[node]
-        print(G.nodes[node])
 
     for node in H.nodes:
         H.nodes[node]['label'] = H.degree[node]
+
+    # Iterações do teste de Weisfeiler
+    for i in range(k):
+        weisfeiler_lehman_iter(G)
+        weisfeiler_lehman_iter(H)
+
+        # Check if labels are equal
+        # A fazer
+
+
 
     return True
 
@@ -29,4 +55,4 @@ G.add_edges_from([(1, 2), (1, 4), (1, 5), (2, 3), (2, 6), (3, 4), (3, 7), (4, 8)
 H = nx.Graph()
 H.add_edges_from([(1, 5), (1, 6), (1, 7), (2, 5), (2, 6), (2, 8), (3, 5), (3, 7), (3, 8), (4, 6), (4, 7), (4, 8)])
 
-print(weisfeiler_lehman_test(G, H))
+print(weisfeiler_lehman_test(G, H, 10))
